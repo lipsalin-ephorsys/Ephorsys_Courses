08f30da016d76912559eebc16fb9fee51e5c69c2
lipsa <bhuyanlipsalin07@gmail.com>
2026-08-18 10:10:53 +0530

    changes commited

--- src/pages/course/CourseOutcome.jsx
+++ src/pages/course/CourseOutcome.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const CourseOutcome = () => {
     return (
         <>
-            <div className="px-6 py-12 md:px-16">
+            <div className="px-6 py-12 md:px-16 bg-[#70C900] w-4xl ml-50 rounded-3xl">
                 <h1 className='text-3xl font-bold text-[#020716]'>Course Outcomes</h1>
+
+                <div className=''>
                 <ul className="mt-6 space-y-3 text-[#020716]">
                     <li>Build responsive and interactive web applications using React.</li>
                     <li>Develop scalable REST APIs using Node.js and Express.js.</li>
@@ -18,6 +20,7 @@ const CourseOutcome = () => {
                     <li>Build practical projects for a professional portfolio.</li>
                     <li>Gain the skills and confidence needed for entry-level full-stack development roles</li>
                 </ul>
+                </div>
             </div>
         </>
     )
