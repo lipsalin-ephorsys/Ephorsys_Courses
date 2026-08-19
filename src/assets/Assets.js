b9e7140e4d1bdfa1438a8efb91aa23a478f7124e
Ananya Pradhan <pradhanananya013@gmail.com>
2026-08-19 15:01:57 +0530

    Herosection

--- src/assets/Assets.js
+++ src/assets/Assets.js
@@ -15,6 +15,10 @@ import dataanalyst from './dataanalyst.png'
 import award from "./award.png"
 import award1 from "./award1.png"
 import award2 from "./award2.png"
+import course from "./course.png"
+import course1 from "./course1.png"
+import course2 from "./course2.png"
+import course3 from "./course3.png"
 
 
 
@@ -34,8 +38,11 @@ export const images={
     dataanalyst,
     award,
     award1,
-    award2
-
+    award2,
+    course,
+    course1,
+    course2,
+    course3
 
 }
 
